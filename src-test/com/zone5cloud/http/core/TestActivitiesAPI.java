d272fe9416e8f33dff4256936d66456de5c17142
Jean Hall <jean@todaysplan.com.au>
2020-10-14 15:00:31 +1100

    SJ-8 Added regsiterDeviceWithThirdParty, deregisterDeviceWithThirdParty and getDeprecated

--- src-test/com/zone5cloud/http/core/TestActivitiesAPI.java
+++ src-test/com/zone5cloud/http/core/TestActivitiesAPI.java
@@ -220,7 +220,7 @@ public class TestActivitiesAPI extends BaseTest {
 			assertTrue(results.getResult().getResults().get(i).getStartTs() <= results.getResult().getResults().get(i-1).getStartTs());
 		
 		// Get the next batch of 10
-		if (results.getCnt() >= 10) {
+		if (results.getCnt() > 10) {
 			results = api.next(10, 10).get().getResult();
 			assertTrue(results.getResult().getResults().size() <= 10);
 			for(int i=1;i<results.getResult().getResults().size();i++)
--- src/com/zone5cloud/http/core/api/ThirdPartyConnectionsAPI.java
+++ src/com/zone5cloud/http/core/api/ThirdPartyConnectionsAPI.java
@@ -4,14 +4,17 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.Future;
 
+import com.zone5cloud.core.Types;
+import com.zone5cloud.core.enums.UserConnectionsType;
+import com.zone5cloud.core.thirdpartyconnections.PushRegistration;
+import com.zone5cloud.core.thirdpartyconnections.PushRegistrationResponse;
+import com.zone5cloud.core.thirdpartyconnections.ThirdPartyToken;
+import com.zone5cloud.core.thirdpartyconnections.ThirdPartyTokenResponse;
+import com.zone5cloud.core.thirdpartyconnections.UpgradeAvailableResponse;
+import com.zone5cloud.core.users.Users;
 import com.zone5cloud.http.core.AbstractAPI;
 import com.zone5cloud.http.core.responses.Z5HttpResponse;
 import com.zone5cloud.http.core.responses.Z5HttpResponseHandler;
-import com.zone5cloud.core.Types;
-import com.zone5cloud.core.enums.UserConnectionsType;
-import com.zone5cloud.core.thirdpartyconnections.ThirdPartyToken;
-import com.zone5cloud.core.thirdpartyconnections.ThirdPartyTokenResponse;
-import com.zone5cloud.core.users.Users;
 
 /**
  * Endpoints related to setting, retrieving and removing third party OAuth bearer tokens
@@ -21,33 +24,86 @@ public class ThirdPartyConnectionsAPI extends AbstractAPI {
 	
 	private static final String SERVICE_NAME_QUERY_PARAM = "service_name";
 		
-	public Future<Z5HttpResponse<ThirdPartyTokenResponse>> set_third_party_token(UserConnectionsType type, ThirdPartyToken connection) {
-		return set_third_party_token(type, connection, null);
+	public Future<Z5HttpResponse<ThirdPartyTokenResponse>> setThirdPartyToken(UserConnectionsType type, ThirdPartyToken connection) {
+		return setThirdPartyToken(type, connection, null);
 	}
 	
-	public Future<Z5HttpResponse<ThirdPartyTokenResponse>> set_third_party_token(UserConnectionsType type, ThirdPartyToken connection, Z5HttpResponseHandler<ThirdPartyTokenResponse> handler) {
+	public Future<Z5HttpResponse<ThirdPartyTokenResponse>> setThirdPartyToken(UserConnectionsType type, ThirdPartyToken connection, Z5HttpResponseHandler<ThirdPartyTokenResponse> handler) {
 		Map<String, Object> queryParams = new HashMap<>(1);
 		queryParams.put(SERVICE_NAME_QUERY_PARAM, type);
 		return getClient().doPost(Types.THIRD_PARTY_TOKEN_RESPONSE, Users.SET_THIRD_PARTY_CONNECTION, connection, queryParams, handler);
 	}
 	
-	public Future<Z5HttpResponse<ThirdPartyTokenResponse>> has_third_party_token(UserConnectionsType type) {
-		return has_third_party_token(type, null);
+	public Future<Z5HttpResponse<ThirdPartyTokenResponse>> hasThirdPartyToken(UserConnectionsType type) {
+		return hasThirdPartyToken(type, null);
 	}
 	
-	public Future<Z5HttpResponse<ThirdPartyTokenResponse>> has_third_party_token(UserConnectionsType type, Z5HttpResponseHandler<ThirdPartyTokenResponse> handler) {
+	public Future<Z5HttpResponse<ThirdPartyTokenResponse>> hasThirdPartyToken(UserConnectionsType type, Z5HttpResponseHandler<ThirdPartyTokenResponse> handler) {
 		Map<String, Object> queryParams = new HashMap<>(1);
 		queryParams.put(SERVICE_NAME_QUERY_PARAM, type);
 		return getClient().doGet(Types.THIRD_PARTY_TOKEN_RESPONSE, Users.HAS_THIRD_PARTY_CONNECTION, queryParams, handler);
 	}
 	
-	public Future<Z5HttpResponse<ThirdPartyTokenResponse>> remove_third_party_token(UserConnectionsType type) {
-		return remove_third_party_token(type, null);
+	public Future<Z5HttpResponse<ThirdPartyTokenResponse>> removeThirdPartyToken(UserConnectionsType type) {
+		return removeThirdPartyToken(type, null);
 	}
 	
-	public Future<Z5HttpResponse<ThirdPartyTokenResponse>> remove_third_party_token(UserConnectionsType type, Z5HttpResponseHandler<ThirdPartyTokenResponse> handler) {
+	public Future<Z5HttpResponse<ThirdPartyTokenResponse>> removeThirdPartyToken(UserConnectionsType type, Z5HttpResponseHandler<ThirdPartyTokenResponse> handler) {
 		Map<String, Object> queryParams = new HashMap<>(1);
 		queryParams.put(SERVICE_NAME_QUERY_PARAM, type);
 		return getClient().doPost(Types.THIRD_PARTY_TOKEN_RESPONSE, Users.REM_THIRD_PARTY_CONNECTION, null, queryParams, handler);
+	}
+	
+	/** 
+	 * Register a push token for a device with a 3rd party
+	 * @param PushRegistration (token, platform, deviceId)
+	 **/
+	public Future<Z5HttpResponse<PushRegistrationResponse>> registerDeviceWithThirdParty(PushRegistration registration) {
+		return registerDeviceWithThirdParty(registration, null);
+	}
+	
+	/** 
+	 * Register a push token for a device with a 3rd party
+	 * @param PushRegistration (token, platform, deviceId)
+	 * @param handler: callback on asynchronous completion
+	 **/
+	public Future<Z5HttpResponse<PushRegistrationResponse>> registerDeviceWithThirdParty(PushRegistration registration, Z5HttpResponseHandler<PushRegistrationResponse> handler) {
+		return getClient().doPost(Types.PUSH_REGISTRATION_RESPONSE, Users.REGISTER_DEVICE_THIRD_PARTY_CONNECTION, registration, null, handler);
+	}
+	
+    /** 
+     * Deregister a push token for a device with a 3rd party 
+     * @param 3rd party push token to deregister
+     * @returns Void response. It response status is OK then the registration was deleted.
+     * Response will be 400 if the registration does not exist or if the user does not have permission to delete it.
+     */
+	public Future<Z5HttpResponse<Void>> deregisterDeviceWithThirdParty(String token) {
+		return deregisterDeviceWithThirdParty(token, null);
+	}
+	
+	/** 
+	 * Deregister a push token for a device with a 3rd party 
+     * @param 3rd party push token to deregister
+     * @returns Void response. It response status is OK then the registration was deleted.
+     * Response will be 400 if the registration does not exist or if the user does not have permission to delete it.
+	 * @param handler: callback on asynchronous completion
+	 **/
+	public Future<Z5HttpResponse<Void>> deregisterDeviceWithThirdParty(String token, Z5HttpResponseHandler<Void> handler) {
+		return getClient().doDelete(Types.VOID, Users.DEREGISTER_DEVICE_THIRD_PARTY_CONNECTION.replace("{token}", token), handler);
+	}
+	
+	/**
+	 * Query whether an upgrade is available for the current user agent (client app).
+	 */
+	public Future<Z5HttpResponse<UpgradeAvailableResponse>> getDeprecated() {
+		return getDeprecated(null);
+	}
+	
+	/**
+	 * Query whether an upgrade is available for the current user agent (client app).
+	 * @param handler: callback on asynchronous completion
+	 */
+	public Future<Z5HttpResponse<UpgradeAvailableResponse>> getDeprecated(Z5HttpResponseHandler<UpgradeAvailableResponse> handler) {
+		return getClient().doGet(Types.UPGRADE_AVAILABLE_RESPONSE, Users.GET_DEPRECATED, handler);
 	}
 }
